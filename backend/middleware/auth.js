ec621558bda87329fe0d87889936a69e18dd5ed2
t09Simi <t09s22@abdn.ac.uk>
2025-10-16 11:36:53 +0100

    feat:add JWT authentication

---
+++ backend/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function verifyToken(req, res, next) {
+
+    const authHeader = req.headers['authorization']
+
+    if(!authHeader) return res.status(401).json({ error: 'No token provided'})
+
+    const token = authHeader.split(' ')[1];
+
+    try{
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next();
+    } catch (err){
+        return res.status(403).json({ error: 'Invalid token'})
+    }
+}
+
+function isAdmin(req, res, next){
+    if(req.user.role !== 'ADMIN'){
+        return res.status(403).json({ error: 'Access denied, admin only'})
+    }
+    next();
+}
+
+module.exports = { verifyToken, isAdmin };
